c5de770a47dcc8b75bd35bf8e7ee8dc858ea21be
kk9546 <146703235+kk9546@users.noreply.github.com>
2023-10-08 15:01:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SeePremium/JIGAR.git /JIGAR
+  git clone https://github.com/kk9546/MR-KkS.git /MR-KkS
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JIGAR
+  git clone $UPSTREAM_REPO /MR-KkS
 fi
-cd /JIGAR
+cd /MR-KkS
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
